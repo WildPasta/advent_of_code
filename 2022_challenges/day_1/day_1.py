eb6fc1c00dcde17376765cf1da56c06d3dabbc5e
WildPasta <wildpasta>
2022-12-01 17:16:02 +0100

    Pushing template

---
+++ 2022_challenges/day_1/day_1.py
@@ -0,0 +1,24 @@
+# Imports
+import sys
+
+def main():
+    #print(step_one())
+    #print(step_two())
+    pass
+
+######### STEP ONE #########
+def step_one():
+    with open('input.txt','r') as file:
+        for line in file:
+            pass
+    return 0
+
+######### STEP TWO #########
+def step_two():
+    with open('input.txt','r') as file:
+        for line in file:
+            pass
+    return 0
+    
+if __name__ == "__main__":
+    sys.exit(main())
